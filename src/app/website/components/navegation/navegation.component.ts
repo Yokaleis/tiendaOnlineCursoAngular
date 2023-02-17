f5661f806df8a1da523d8178a72bce2a5ecd94ae
Yokasta <yokaleis@gmail.com>
2023-02-17 15:34:07 -0400

    Deploy firebase

--- src/app/website/components/navegation/navegation.component.ts
+++ src/app/website/components/navegation/navegation.component.ts
@@ -14,7 +14,7 @@ import { User } from "../../../models/users.models";
 @Component({
   selector: 'app-navegation',
   templateUrl: './navegation.component.html',
-  styleUrls: ['./navegation.component.css']
+  styleUrls: ['./navegation.component.css', './navegation.components02.css'],
 })
 export class NavegationComponent implements OnInit {
 
